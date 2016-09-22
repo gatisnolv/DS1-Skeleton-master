8594cf362a4260c580d1798547d7d35c1cd65fbd
gatisnolv <gatisnolv@gmail.com>
2016-09-23 00:18:27 +0200

    Add comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,30 +8,67 @@ import java.util.Scanner;
 public class Main implements DS1Interface  {
 
 
-    /* Implement these methods */
+	/* General comments that apply to all methods:
+ 	 * 	1) Variable initialization takes constant time.
+	 * 	2) We tried to keep the algorithms as close to the provided (book and lectures) pseudo-code as possible,
+	 *	so some of the for-loops have a counter that is decreasing, which is not typical of prior programming 
+	 *	assignments, but nevertheless works correctly
+	 *	3) The pseudo-code of the algorithms assumes that the first element of an array has index '1', whereas 
+	 *	in Java arrays start with the index '0', so where relevant we initialized counters in loops that 
+	 *	are 1 less than those used in the pseudo-code (loops that have an increasing counter), also the 
+	 *	computation of child nodes in heapSort is slightly different because of this
+	 *	4) Some of the pseudo-code algorithms call for 'flooring' of a non-integer result of a division, in 
+	 *	Java this is easy because the result of dividing two int's dismisses the remainder of the division
+	 */
 
+	
     @Override
     public int[] insertionSort(int[] input) {
-        for(int j=1;j<input.length;j++){
-        	int key=input[j];//initializations happen in constant time
+        for(int j=1;j<input.length;j++){//test of for-loop happens n times
+        	int key=input[j];
         	int i=j-1;
-        	while(i>=0 && input[i]>key){
+        	while(i>=0 && input[i]>key){//worst case: input[i]>key always succeeds, which makes insertionSort take time, which is quadratic in input size
         		input[i+1]=input[i];
         		i=i-1;
         	}
         	input[i+1]=key;
         }
     	return input;
-    }
+    }//because insertionSort takes time, which is quadratic in input size, its worst case time complexity is in O(n^2)
 
+
+    /* 	General comments about our mergeSort implementation:
+     * Because the signature of the mergeSort method had a different signature from the pseudo-code,
+     * we adapted our implementation to account for that (the required implementation only takes an 
+     * integer array as input, as opposed to the pseudo-code method which takes the initial array, 
+     * and pointers about which region to sort).
+     * This meant that we had to pass in the recursive calls arrays that are copies of the regions, to be sorted.
+     * 	For the purpose of making copies of the relevant regions we created a helper method copySubArray, 
+     * which returns an array which is a copy of the specified region.
+     * 	Because of the difference in mergeSort implementation, we also needed to adapt the 'merge' method. It 
+     * differs from the pseudo-code in that it does not take the initial array along with pointers to the 
+     * regions to be merged, but instead takes two sorted arrays which are copies of the regions and returns 
+     * an array which contains all of the elements of the passed arrays and is sorted instead of returning the 
+     * original array with a sorted region.
+     * 	The use of the copySubArray method does not introduce differences to the time complexity or memory use 
+     * of the implementation, because similarly, the same operation of copying the regions to temporary arrays 
+     * happens within the 'merge' method in the pseudo-code.
+     * 	Our implementation does not use sentinels in the 'merge' method, this is circumvented by using additional 
+     * checks in the if-statement when deciding from which array to copy
+     * 
+     * 	Because mergeSort calls itself recursively on the halves of the input array, the height of the recursion 
+     * tree is ~log(n) (for n=2^1 log(n)+1).
+     * 	Work per level is in the Theta(n).
+     *  This results in the time complexity of mergeSort being in Theta(n*log(n))
+     */
 
     @Override
     public int[] mergeSort(int[] input) {
     	if(input.length>1){
-        	int q=input.length/2;
-        	int[] left=mergeSort(copySubArray(input,0,q));
-        	int[] right=mergeSort(copySubArray(input,q,input.length));
-        	input=merge(left, right);
+        	int q=input.length/2;//indicates the middle (floor of length/2)
+        	int[] left=mergeSort(copySubArray(input,0,q));//mergeSort 'half' of the array from index 0 to q (excluded)
+        	int[] right=mergeSort(copySubArray(input,q,input.length));//mergeSort other 'half' of the array from q to the end of array
+        	input=merge(left, right);//merges the two 'halves'
         }
     	return input;
     }
@@ -49,7 +86,7 @@ public class Main implements DS1Interface  {
     	int l=0;
     	int r=0;
     	for(int i=0;i<output.length;i++){
-    		if(l<left.length && r<right.length && left[l]<=right[r] || r==right.length){
+    		if(l<left.length && r<right.length && left[l]<=right[r] || r==right.length){//the additional checks for the counters allow us to avoid using sentinels
     			output[i]=left[l];
     			l+=1;
     		}else{
@@ -60,13 +97,38 @@ public class Main implements DS1Interface  {
     	return output;
     }
 
-	void buildMaxHeap(int[] heap){
+    /* 	General comments about our heapSort implementation:
+     * We implemented heapSort very close to the pseudo-code of the algorithm, hence there are no space or 
+     * time complexity differences. 
+     * The only difference is that our maxHeapify implementation in addition to the parameters in the 
+     * pseudo-code, also takes heapSize as a parameter, where as the pseudo-code assumes this to be a 
+     * property of the input array.
+     * 
+     * Becase of the for-loop and the time complexity of maxHeapify, the time complexity of heapSort is in O(n*log(n)),
+     * (details below)
+     */
+    
+    @Override
+    public int[] heapSort(int[] input) {
+    	buildMaxHeap(input);//worst case running time in O(n), same as the pseudo-code
+    	int heapSize=input.length;
+    	for(int i=input.length;i>1;i--){//n-1 calls of maxHeapify
+    		int temp=input[0];//the following 3 lines swap the first element of a max-heap with the last
+    		input[0]=input[i-1];
+    		input[i-1]=temp;
+    		heapSize=heapSize-1;
+    		maxHeapify(input, 0, heapSize);
+    	}
+        return input;
+    }
+    
+    void buildMaxHeap(int[] heap){
     	for(int i=heap.length/2-1;i>=0;i--){
     		maxHeapify(heap, i, heap.length);
     	}
     }
 	    
-    void maxHeapify(int[] heap, int i, int heapSize){
+    void maxHeapify(int[] heap, int i, int heapSize){//time complexity determined by height of heap, thus O(log(n))
     	int l=2*(i+1)-1;
     	int r=2*(i+1);
     	int largest;
@@ -79,27 +141,12 @@ public class Main implements DS1Interface  {
     		largest=r;
     	}
     	if(largest!=i){
-    		int temp=heap[i];
+    		int temp=heap[i];//the following 3 lines swap the i element with the largest
     		heap[i]=heap[largest];
     		heap[largest]=temp;
     		maxHeapify(heap, largest, heapSize);
     	}
     }
-
-//	    @Override
-    public int[] heapSort(int[] input){
-    	buildMaxHeap(input);
-    	int heapSize=input.length;
-    	for(int i=input.length;i>1;i--){
-    		int temp=input[0];
-    		input[0]=input[i-1];
-    		input[i-1]=temp;
-    		heapSize=heapSize-1;
-    		maxHeapify(input, 0, heapSize);
-    	}
-        return input;
-    }
-
 
     /* BEGIN UTIL FUNCTION. DO NOT TOUCH */
 
